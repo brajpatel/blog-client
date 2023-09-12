853e3a5722ca3bbd4046b7ebbf11ffd64af21f9e
brajpatel <brajeshpatel0002@gmail.com>
2023-09-12 20:19:53 +0100

    Create RouteSwitch component to handle routes

---
+++ src/RouteSwitch.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar.jsx';
+import Posts from './components/Posts.jsx';
+
+function RouteSwitch() {
+  return (
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        {/* <Route path="/" element={<Welcome/>}/> */}
+        <Route path="/posts" element={<Posts/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default RouteSwitch;
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar.jsx';
-import App from './App.jsx';
-import Posts from './components/Posts.jsx';
+import RouteSwitch from './RouteSwitch.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<App/>}/>
-        <Route path="/posts" element={<Posts/>}/>
-      </Routes>
-    </BrowserRouter>
+    <RouteSwitch/>
   </React.StrictMode>,
 )
