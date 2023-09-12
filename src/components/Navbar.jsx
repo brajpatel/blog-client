ab2680db38f5f8e04dee7fd53fb42f319da1318b
brajpatel <brajeshpatel0002@gmail.com>
2023-09-12 16:36:16 +0100

    Initialised app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <nav>
+            <h1>Blog Client</h1>
+            <ul>
+                <li>One</li>
+                <li>Two</li>
+                <li>Three</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
