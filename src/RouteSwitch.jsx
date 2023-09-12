d6a79bb00341f17afcd991b0dbe1560e7eff48e6
brajpatel <brajeshpatel0002@gmail.com>
2023-09-12 20:21:11 +0100

    Create welcome component

--- src/RouteSwitch.jsx
+++ src/RouteSwitch.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
+import Welcome from './components/Welcome';
 import Posts from './components/Posts.jsx';
 
 function RouteSwitch() {
@@ -8,7 +9,7 @@ function RouteSwitch() {
     <BrowserRouter>
       <Navbar/>
       <Routes>
-        {/* <Route path="/" element={<Welcome/>}/> */}
+        <Route path="/" element={<Welcome/>}/>
         <Route path="/posts" element={<Posts/>}/>
       </Routes>
     </BrowserRouter>
