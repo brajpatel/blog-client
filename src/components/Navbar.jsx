ce9dbc026ebb4b5583457e1df8bdf45416386282
brajpatel <brajeshpatel0002@gmail.com>
2023-09-13 17:24:32 +0100

    Add icons to nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,18 @@ function Navbar() {
         <nav>
             <h1>Blog Client</h1>
             <ul>
-                <Link to="/">Home</Link>
-                <Link to="posts">Posts</Link>
-                <Link to="login">Login</Link>
+                <Link className='nav-link' to="/">
+                    <p>Home</p>
+                    <i className="fa-solid fa-house"></i>
+                </Link>
+                <Link className='nav-link' to="posts">
+                    <p>Posts</p>
+                    <i class="fa-solid fa-book-open-reader"></i>
+                </Link>
+                <Link className='nav-link' to="login">
+                    <p>Login</p>
+                    <i className="fa-solid fa-right-to-bracket"></i>
+                </Link>
             </ul>
         </nav>
     )
