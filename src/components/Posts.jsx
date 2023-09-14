f78f6b1810ae83ed38f92451cd8d43da899de542
brajpatel <brajeshpatel0002@gmail.com>
2023-09-14 11:15:45 +0100

    Create scrollable gallery for posts

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -6,7 +6,7 @@ function Posts() {
         <div className="all-posts">
             <h2>All Posts</h2>
             <div className="posts">
-                <Link to="/posts/post-one" className='link'>
+                <Link to="/posts/post-one" className='link post-link'>
                     <div className='post-thumbnail'>
                         <img src="https://c4.wallpaperflare.com/wallpaper/175/524/956/digital-digital-art-artwork-fantasy-art-drawing-hd-wallpaper-preview.jpg" alt="sample-image"/>
                         <div className='post-info'>
@@ -15,7 +15,25 @@ function Posts() {
                     </div>
                 </Link>
 
-                <Link to="/posts/post-one" className='link'>
+                <Link to="/posts/post-one" className='link post-link'>
+                    <div className='post-thumbnail'>
+                        <img src="https://c4.wallpaperflare.com/wallpaper/184/515/626/digital-digital-art-artwork-illustration-drawing-hd-wallpaper-preview.jpg" alt="sample-image"/>
+                        <div className='post-info'>
+                            <p className='thumbnail-title'>post one title</p>
+                        </div>
+                    </div>
+                </Link>
+
+                <Link to="/posts/post-one" className='link post-link'>
+                    <div className='post-thumbnail'>
+                        <img src="https://c4.wallpaperflare.com/wallpaper/767/612/930/nature-landscape-trees-digital-art-wallpaper-preview.jpg" alt="sample-image"/>
+                        <div className='post-info'>
+                            <p className='thumbnail-title'>post one title</p>
+                        </div>
+                    </div>
+                </Link>
+
+                <Link to="/posts/post-one" className='link post-link'>
                     <div className='post-thumbnail'>
                         <img src="https://c4.wallpaperflare.com/wallpaper/175/524/956/digital-digital-art-artwork-fantasy-art-drawing-hd-wallpaper-preview.jpg" alt="sample-image"/>
                         <div className='post-info'>
@@ -24,25 +42,25 @@ function Posts() {
                     </div>
                 </Link>
 
-                <Link to="/posts/post-one" className='link'>
+                <Link to="/posts/post-one" className='link post-link'>
                     <div className='post-thumbnail'>
-                        <img src="https://c4.wallpaperflare.com/wallpaper/175/524/956/digital-digital-art-artwork-fantasy-art-drawing-hd-wallpaper-preview.jpg" alt="sample-image"/>
+                        <img src="https://c4.wallpaperflare.com/wallpaper/184/515/626/digital-digital-art-artwork-illustration-drawing-hd-wallpaper-preview.jpg" alt="sample-image"/>
                         <div className='post-info'>
                             <p className='thumbnail-title'>post one title</p>
                         </div>
                     </div>
                 </Link>
 
-                <Link to="/posts/post-one" className='link'>
+                <Link to="/posts/post-one" className='link post-link'>
                     <div className='post-thumbnail'>
-                        <img src="https://c4.wallpaperflare.com/wallpaper/175/524/956/digital-digital-art-artwork-fantasy-art-drawing-hd-wallpaper-preview.jpg" alt="sample-image"/>
+                        <img src="https://c4.wallpaperflare.com/wallpaper/767/612/930/nature-landscape-trees-digital-art-wallpaper-preview.jpg" alt="sample-image"/>
                         <div className='post-info'>
                             <p className='thumbnail-title'>post one title</p>
                         </div>
                     </div>
                 </Link>
 
-                <Link to="/posts/post-one" className='link'>
+                <Link to="/posts/post-one" className='link post-link'>
                     <div className='post-thumbnail'>
                         <img src="https://c4.wallpaperflare.com/wallpaper/175/524/956/digital-digital-art-artwork-fantasy-art-drawing-hd-wallpaper-preview.jpg" alt="sample-image"/>
                         <div className='post-info'>
