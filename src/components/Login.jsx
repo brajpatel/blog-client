cb74f0ee5c0b63898b219ed89a5a8d1642fa27e1
brajpatel <brajeshpatel0002@gmail.com>
2023-09-14 17:09:12 +0100

    Create login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,26 @@ import './Login.css';
 
 function Login() {
     return (
-        <div className="login">Login</div>
+        <div>
+            <div className='login-modal'>
+                <h2>Login</h2>
+                <div className='login-image-container'>
+                    <img src="https://icons-for-free.com/download-icon-facebook+profile+user+profile+icon-1320184041317225686_512.png" alt="login-default-image"/>
+                </div>
+                <p className='note'>Note: You need admin details to be able to login</p>
+                <form action="">
+                    <div className='input-container'>
+                        <label htmlFor="username">Username</label>
+                        <input id='username' type="text" required/>
+                    </div>
+                    <div className='input-container'>
+                        <label htmlFor="password">Password</label>
+                        <input id='password' type="password" required/>
+                    </div>
+                    <button className='login-btn' type='submit'>Login</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
