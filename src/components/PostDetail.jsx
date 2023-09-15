1a9470f68fc902bd4f55a381262292d7d7021275
brajpatel <brajeshpatel0002@gmail.com>
2023-09-15 11:18:58 +0100

    Fade bottom of post image

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -3,7 +3,7 @@ import './PostDetail.css';
 function PostDetail() {
     return (
         <div className='post-detail'>
-            <img src="https://c4.wallpaperflare.com/wallpaper/175/524/956/digital-digital-art-artwork-fantasy-art-drawing-hd-wallpaper-preview.jpg" alt="sample-image"/>
+            <img src="https://c4.wallpaperflare.com/wallpaper/767/612/930/nature-landscape-trees-digital-art-wallpaper-preview.jpg" alt="sample-image"/>
         </div>
     )
 }
