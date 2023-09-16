0a77f13f634604d7bef381e28132e89e6d4601ba
brajpatel <brajeshpatel0002@gmail.com>
2023-09-16 20:38:42 +0100

    Create comments section

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -6,6 +6,20 @@ function PostDetail() {
             <img src="https://c4.wallpaperflare.com/wallpaper/767/612/930/nature-landscape-trees-digital-art-wallpaper-preview.jpg" alt="sample-image"/>
             <div className='post-detail'>
                 <h2>Post Title - 23/04/09 - 10:38:08</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic fugit sed ut accusamus nobis. Dignissimos excepturi eius illo nulla perferendis!
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere sint aliquid cumque, fuga, officia libero cum modi iusto harum suscipit veniam quas, numquam quos aperiam distinctio eum! Quas, quia inventore.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum sapiente necessitatibus nihil eum quis sunt saepe, nulla culpa expedita soluta eligendi libero vel qui animi illo porro. Dolore at libero cumque aliquid natus quisquam vel recusandae, maxime mollitia porro dolorum iste, suscipit itaque. Non minus repudiandae quos voluptatibus, asperiores obcaecati!
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda, quam?
+                </p>
+            </div>
+
+            <div className='comments'>
+                <h2>Comments</h2>
+                <div className='comment'>
+                    <p>Username - date</p>
+                    <p>Comment text</p>
+                </div>
+                <p className='no-comments'>This post has no comments</p>
             </div>
         </div>
     )
