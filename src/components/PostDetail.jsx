beb9483123b1eaae8fe9b172b2ea0005678228b4
brajpatel <brajeshpatel0002@gmail.com>
2023-09-16 20:21:55 +0100

    Create container for post details

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -4,6 +4,9 @@ function PostDetail() {
     return (
         <div className='post-detail-container'>
             <img src="https://c4.wallpaperflare.com/wallpaper/767/612/930/nature-landscape-trees-digital-art-wallpaper-preview.jpg" alt="sample-image"/>
+            <div className='post-detail'>
+                <h2>Post Title - 23/04/09 - 10:38:08</h2>
+            </div>
         </div>
     )
 }
