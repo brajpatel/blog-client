a4587b20ac246f6889e8afeff5614f61dc18a375
brajpatel <brajeshpatel0002@gmail.com>
2023-09-23 20:14:30 +0100

    Add filler content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,8 +24,9 @@ function Home() {
         <div className="home">
             <div>
                 <h2>Home</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, quae.</p>
-                <p>Some stuff</p>
+                <p>Open your mind and explore different perspectives</p>
+                <p>Like Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab illum aperiam provident consequatur excepturi nesciunt!</p>
+                <p>And Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex, quo nam, asperiores dolorum velit esse impedit saepe repellendus porro sint, quidem laborum sapiente amet.</p>
             </div>
 
             <div>
