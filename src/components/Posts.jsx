3250cd72d9590aefa62c5d1b9f5f108b0c23c091
brajpatel <brajeshpatel0002@gmail.com>
2023-09-28 20:48:07 +0100

    Fetch posts from blog api

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -5,24 +5,26 @@ import Loader from './Loader';
 import PostCard from './PostCard';
 
 function Posts() {
-    const [displayLoader, setDisplayLoader] = useState(false);
-    const [posts, setPosts] = useState(true);
+    const [displayLoader, setDisplayLoader] = useState(true);
+    const [posts, setPosts] = useState(null);
     
-    // useEffect(() => {
-    //     getPosts();
-    // }, [])
-
-    // const getPosts = async () => {
-    //     const req = await fetch('https://young-smoke-1917.fly.dev/posts', { mode: 'cors' });
-
-    //     if(req.status !== 200) {
-    //         return;
-    //     }
-    //     else {
-    //         const postsData = await req.json();
-    //         setFeaturedPosts(postsData);
-    //     }
-    // }
+    useEffect(() => {
+        fetch('https://young-smoke-1917.fly.dev/posts')
+            .then((response) => {
+                if(response.ok) {
+                    return response.json();
+                }
+            })
+            .then((data) => {
+                setPosts(data);
+            })
+            .catch((err) => {
+                console.error("Error fetching posts:", err);
+            })
+            .finally(() => {
+                setDisplayLoader(false);
+            })
+    }, [])
 
     return (
         <div className="all-posts">
@@ -31,59 +33,11 @@ function Posts() {
 
                 {posts && (
                     <div className="posts">
-                        <Link to="/posts/post-one" className='link post-link'>
-                            <PostCard title="post one title" image="https://c4.wallpaperflare.com/wallpaper/175/524/956/digital-digital-art-artwork-fantasy-art-drawing-hd-wallpaper-preview.jpg"/>
-                        </Link>
-
-                        <Link to="/posts/post-one" className='link post-link'>
-                            <div className='post-card'>
-                                <img src="https://c4.wallpaperflare.com/wallpaper/184/515/626/digital-digital-art-artwork-illustration-drawing-hd-wallpaper-preview.jpg" alt="sample-image"/>
-                                <div className='overlay'></div>
-                                <p className='thumbnail-title'>post two title</p>
-                            </div>
-                        </Link>
-
-                        <Link to="/posts/post-one" className='link post-link'>
-                            <div className='post-card'>
-                                <img src="https://c4.wallpaperflare.com/wallpaper/767/612/930/nature-landscape-trees-digital-art-wallpaper-preview.jpg" alt="sample-image"/>
-                                <div className='overlay'></div>
-                                <p className='thumbnail-title'>post three title</p>
-                            </div>
-                        </Link>
-
-                        <Link to="/posts/post-one" className='link post-link'>
-                            <div className='post-card'>
-                                <img src="https://c4.wallpaperflare.com/wallpaper/175/524/956/digital-digital-art-artwork-fantasy-art-drawing-hd-wallpaper-preview.jpg" alt="sample-image"/>
-                                <div className='overlay'></div>
-                                <p className='thumbnail-title'>post one title</p>
-                            </div>
-                        </Link>
-
-                        <Link to="/posts/post-one" className='link post-link'>
-                            <div className='post-card'>
-                                <img src="https://c4.wallpaperflare.com/wallpaper/184/515/626/digital-digital-art-artwork-illustration-drawing-hd-wallpaper-preview.jpg" alt="sample-image"/>
-                                <div className='overlay'></div>
-                                <p className='thumbnail-title'>post two title</p>
-                            </div>
-                        </Link>
-
-                        <Link to="/posts/post-one" className='link post-link'>
-                            <div className='post-card'>
-                                <img src="https://c4.wallpaperflare.com/wallpaper/767/612/930/nature-landscape-trees-digital-art-wallpaper-preview.jpg" alt="sample-image"/>
-                                <div className='overlay'></div>
-                                <p className='thumbnail-title'>post three title</p>
-                            </div>
-                        </Link>
-
-                        <Link to="/posts/post-one" className='link post-link'>
-                            <div className='post-card'>
-                                <img src="https://c4.wallpaperflare.com/wallpaper/175/524/956/digital-digital-art-artwork-fantasy-art-drawing-hd-wallpaper-preview.jpg" alt="sample-image"/>
-                                <div className='overlay'></div>
-                                <p className='thumbnail-title'>post one title</p>
-                            </div>
-                        </Link>
-                </div>
-            )}
+                        {posts.map((post) => {
+                            
+                        })}
+                    </div>
+                )}
         </div>
     )
 }
